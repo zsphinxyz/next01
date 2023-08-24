d8920940a497968504b703df79c4b5b57415cbfc
zsphinxyz <soe.thiha.lwin.5@gmail.com>
2023-08-24 23:03:37 +0630

    add year(room) column

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
+    <main className="flex min-w-screen min-h-screen flex-col items-center justify-center">
      <p className='text-sm'>Home</p>
     <span className="text-4xl font-black text-gray-300">PAGE</span> 
     <div className="flex flex-col">
--- pages/att/index.tsx
+++ pages/att/index.tsx
@@ -8,8 +8,9 @@ import { useEffect, useState } from 'react';
 export default function Att() {
 	const [year, setYear] = useState('all');
 	const [room, setRoom] = useState<number[]>([]);
-	const [r, setR] = useState('');
+	const [r, setR] = useState('1');
 	const [search, setSearch] = useState('')
+	const [yearcol, setYearcol] = useState(false)
 
 	let c = 0;
 
@@ -52,7 +53,11 @@ export default function Att() {
 			))
 		}
 	  </select>
-
+		<input 	type='checkbox' 
+						checked={yearcol}
+						className="ml-8"
+						onChange={() => setYearcol(!yearcol)}
+		/> Add Room Colum
 		<br />
 		{
 			room.map(i => (
@@ -79,12 +84,17 @@ export default function Att() {
 		/>
 		<br />
 		&gt;&gt; {search}
-		<table>
+
+		<table className='font-serif'>
 		
 		<thead>
 		 <tr>
-			<th>No</th>
-			<th>Name</th>
+			<th className="border">No</th>
+			<th className="border">Name</th>
+			{
+				yearcol &&
+				<th className="border px-2">Year (Room)</th>
+			}
 		</tr>
 		</thead>
 
@@ -104,8 +114,12 @@ export default function Att() {
 			c++;
 			return(
 		<tr key={i.id}>
-				<td className='border'>{c}</td>
-				<td className='border'> {i.name} </td>
+				<td className='border px-2'>{c}</td>
+				<td className='border px-2'> {i.name} </td>
+				{
+					yearcol &&
+					<td className='border px-2 min-w-fit'>{i.year} ({i.room})</td>
+				}
 		</tr>
 			)})
 
